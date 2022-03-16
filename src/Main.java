5659e326ea521240ff21f775e7c4b9265f0f06b4
AliSaid-dr <77389959+AliSaid-dr@users.noreply.github.com>
2022-03-16 11:26:49 +0200

    Project commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String []args) {
+        System.out.println("This is for USP");
+        
+        System.out.println("This is the first commit text");
+    }
+}
